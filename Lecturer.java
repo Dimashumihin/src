edf7105b9e3a60d0540f2a0969e5fb88671d2d2b
Kezhevato <64834558+Kezhevato@users.noreply.github.com>
2020-05-24 12:41:09 +0300

    Update Lecturer.java

--- Lecturer.java
+++ Lecturer.java
@@ -1,41 +1,31 @@
-public class Lecturer {
+import java.util.ArrayList;
 
-	private String name;
-	private String emailAddress;
-	private String specialization;
+class Lecturer {
 
-	/**
-	 * 
-	 * @param name
-	 * @param emailAddress
-	 * @param specialization
-	 */
-	public Lecturer(String name, String emailAddress, String specialization) {
-		// TODO - implement Lecturer.Lecturer
-		throw new UnsupportedOperationException();
-	}
-
-	public String getSpecialization() {
-		return this.specialization;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void getEmailAddress() {
-		// TODO - implement Lecturer.getEmailAddress
-		throw new UnsupportedOperationException();
-	}
-
-	public void getCourse() {
-		// TODO - implement Lecturer.getCourse
-		throw new UnsupportedOperationException();
-	}
-
-	public void addCourse() {
-		// TODO - implement Lecturer.addCourse
-		throw new UnsupportedOperationException();
-	}
+  private static int count = 0;
+  private int id;
+  private final String name;
+  private ArrayList<Course> courses = new ArrayList<>();
+  
+  public Lecturer(String name) {
+    this.id = ++count;
+    this.name = name;
+  }
+  
+  public int getId() {
+    return this.id;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void addCourse(Course course) {
+    courses.add(course);
+  }
+  
+  public ArrayList<Course> getCourses() {
+    return courses;
+  }
 
 }
